7e53b8756f94405276b7790f5f523ff15ebf2d48
Ricardo-Zozaya <rzozaya343@gmail.com>
2019-09-27 10:39:38 -0500

    actualizacion

---
+++ script.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function(){
+    console.log("hola");
+    var video = document.getElementById("video");
+    var posVideo = video.offsetTop;
+    var alturaVideo = video.offsetHeight;
+    var currentScroll = window.pageYOffset;
+    var windowHeight = window.innerHeight;
+
+    if (posVideo + alturaVideo < currentScroll + windowHeight && 
+        posVideo > currentScroll){
+    video.classList.add("play");
+    }else{
+        video.classList.remove("play");
+    }
+
+/*console.log("posVideo", posVideo);
+console.log("currentScroll", currentScroll);
+    console.log("windowHeight, windowHeight");*/
+
+});
